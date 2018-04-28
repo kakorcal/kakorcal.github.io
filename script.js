f26676e5f818a2d98123d89b916d9bcc3b236d0a
Kenneth Korcal <kakorcal@gmail.com>
2018-04-28 04:05:30 -0700

    adding cards

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ window.onload = init;
 function init() {
     console.log('Page Loaded');
     var body = document.getElementsByTagName('body')[0];
-    var svgStyle = backgroundStyle(body, "url('/img/pattern.svg')", "repeat", "0/20px");
+    var main = document.getElementsByClassName('main')[0];
+    var backgroundCallBack = backgroundStyle(body, "url('/img/pattern.svg')", "repeat", "0/10px");
+    var handleBackgroundColor = changeBackgroundColor(main, backgroundCallBack, [196, 7, 19, 0.7]);
+    main.onscroll = handleBackgroundColor;
+
     // var colorArr = [
     //     [196, 7, 19],
     //     [148, 212, 56],
@@ -11,35 +15,45 @@ function init() {
     //     [128, 18, 146],
     //     [15, 86, 240]  
     // ];
-    // var counter = 0;
-    /* 
-        0. start with 196, 7, 7
-        1. increase g value to 196
-        2. decrease r value to 7
-        3. increase b value to 196
-        4. decrease g value to 7
-        5. increase r value to 196
-        6. decrease b value to 7
-    */
-
-    // var timerId = setInterval(function () {
-    //     svgStyle(colorRGBA(colorArr[counter++]));
-        
-    //     if(counter >= colorArr.length) {
-    //         counter = 0;
-    //     }
-    // }, 1000);
 }
 
 function backgroundStyle(body, url, repeatStyle, positionAndSize) {
     var args = Array.prototype.slice.call(arguments, 1);
-    return function(color) {
+    return function(colorArr) {
+        var rgba = colorArr.join(" ").trim().replace(/ /g, ",");
+        var color = "rgb(" + rgba + ")";
         var style = color + " " + args.join(" ");
         body.style.background = style;
     }
 }
 
-function colorRGBA(arr) {
-    var color = arr.join(" ").trim().replace(/ /g, ",");
-    return "rgb(" + color + ")";
+function changeBackgroundColor(container, backgroundCallBack, initialColor) {
+    var max = 196;
+    var min = 7;
+    var current = initialColor;
+    var r = current[0];
+    var g = current[1];
+    var b = current[2];
+    var a = current[3];
+
+    backgroundCallBack(current);
+    
+    /* 
+        0. start with 196, 7, 7
+        1. increase b value to 196
+        2. decrease r value to 7
+        3. increase g value to 196
+        4. decrease b value to 7
+        5. increase r value to 196
+        6. decrease g value to 7
+    */
+
+    return function() {
+        var position = container.scrollTop;
+        console.log(position);
+        
+
+        current = [r,g,b,a];
+        backgroundCallBack(current);
+    }
 }
