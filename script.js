2ac2717f07270d142fcc950e0de49f5420cb3f50
Kenneth Korcal <kakorcal@gmail.com>
2018-04-29 15:49:53 -0700

    finish scroll events

--- script.js
+++ script.js
@@ -4,31 +4,28 @@ function init() {
     console.log('Page Loaded');
     var body = document.getElementsByTagName('body')[0];
     var header = document.getElementsByClassName('card__header')[0];
-    var ribbons = document.getElementsByClassName('card__ribbon');
-    var elements = [body, header];
+    var navIcons = document.getElementsByClassName('nav__icon');
+    var colorElements = [body, header];
 
     var main = document.getElementsByClassName('main')[0];
-    var backgroundCallBack = backgroundStyle(elements, "url('/img/pattern.svg')", "repeat", "0/20px");
+    var backgroundCallBack = backgroundStyle(colorElements, "url('/img/pattern.svg')", "repeat", "0/20px");
     var handleBackgroundColor = changeBackgroundColor(main, backgroundCallBack, "53, 73, 93");
-    var ribbonCallback = backgroundStyle(ribbons);
+    var handleNavIconToggle = navIconToggle(navIcons);
 
-    initRibbons(ribbons);
+    // set inital data
+    initRibbons();
+    handleBackgroundColor();
+    handleNavIconToggle();
 
     main.onscroll = function () {
         handleBackgroundColor();
         handleNavIconToggle();
     };
-
-    // var colorArr = [
-    //     [196, 7, 19],
-    //     [148, 212, 56],
-    //     [254, 238, 53],
-    //     [128, 18, 146],
-    //     [15, 86, 240]  
-    // ];
 }
 
-function initRibbons(ribbons) {
+// add background color to all ribbons
+function initRibbons() {
+    var ribbons = document.getElementsByClassName('card__ribbon');
     for(var i = 0; i < ribbons.length; i++) {
         var current = ribbons[i];
         // card class
@@ -38,6 +35,7 @@ function initRibbons(ribbons) {
     }
 }
 
+// utility function returning style in rgba format
 function toRGBA(color, backgroundConfig, applyConfigCallback) {
     var styles = "";
     var a = "0.8";
@@ -49,11 +47,11 @@ function toRGBA(color, backgroundConfig, applyConfigCallback) {
     return styles;
 }
 
+// apply / change background color to all specified elements
 function backgroundStyle(elements, url, repeatStyle, positionAndSize) {
     var args = Array.prototype.slice.call(arguments, 1);
     var a = "0.8";
     return function(color) {
-        
         for(var i = 0; i < elements.length; i++) {
             var el = elements[i];
             var styles = toRGBA(color, args, function () { return el.tagName === 'BODY'; });
@@ -62,6 +60,8 @@ function backgroundStyle(elements, url, repeatStyle, positionAndSize) {
     }
 }
 
+// detects if background color needs to be changed
+// applys background changes if element is in view
 function changeBackgroundColor(scrollContainer, backgroundCallBack, initialColor) {
     var current = initialColor;
     backgroundCallBack(current);
@@ -69,22 +69,87 @@ function changeBackgroundColor(scrollContainer, backgroundCallBack, initialColor
     return function() {
         var position = scrollContainer.scrollTop;
         var color = current;
+        var paddingOffset = 20;
+        var extraOffset = 150;
         
+        // find child that is in view (ie getBoundingClientRect().top > 0 + paddingOffset)
+        for(var i = 0; i < scrollContainer.children.length; i++) {
+            var child = scrollContainer.children[i];
+            var clientRect = child.getBoundingClientRect();
+            var distanceRelativeToViewport = clientRect.bottom; 
+            var childHeight = clientRect.height;
+            
+            // check if the child is in view
+            if (distanceRelativeToViewport - paddingOffset - extraOffset > 0 && 
+                distanceRelativeToViewport - paddingOffset - extraOffset < childHeight) {
+                if(!child.classList.contains('active')) {
+                    child.classList.add('active');
+                }
+                color = child.dataset.color;
+            }else {
+                if (child.classList.contains('active')) {
+                    child.classList.remove('active');
+                }
+            } 
+        }
+
+        // change color if diffrent child is in view
+        if (current !== color) {
+            current = color;
+            backgroundCallBack(current);
+        }
+    }
+}
+
+// toggle opacity of icon if card element is in view (ie. active)
+function navIconToggle(navIcons) {
+    return function() {
+        var active = document.getElementsByClassName('active')[0];
+        for(i = 0; i < navIcons.length; i++) {
+            var icon = navIcons[i];
+            if(icon.dataset.nav === active.dataset.nav) {
+                icon.style.opacity = 1;
+            }else {
+                icon.style.opacity = 0;
+            }
+        }
+    };
+}
+
+/* 
+    // var colorArr = [
+    //     [196, 7, 19],
+    //     [148, 212, 56],
+    //     [254, 238, 53],
+    //     [128, 18, 146],
+    //     [15, 86, 240]
+    // ];
+
         for (var i = 0; i < scrollContainer.children.length; i++) {
             var child = scrollContainer.children[i];
             var nextChild = scrollContainer.children[i+1];
+            var childOffset = child.offsetTop;
+            var nextChildOffset = nextChild ? nextChild.offsetTop : child.offsetTop + child.offsetHeight;
 
-            if(nextChild) {
-                if(position + 200 >= child.offsetTop && position + 200 < nextChild.offsetTop) {
-                    color = child.dataset.color;
+            var childBCR = child.getBoundingClientRect();
+            var nextChildBCR = nextChild.getBoundingClientRect();
+            console.log('position', position);
 
-                    // if (!child.classList.contains('active')) {
-                    //     child.classList.add('active');
-                    // }
-                    break;
-                }
-            }else {
+            console.log('child', childBCR);
+            console.log('nextchild', nextChildBCR);
+
+
+            if (position + 200 >= childOffset && position + 200 < nextChildOffset) {
                 color = child.dataset.color;
+
+                if (!child.classList.contains('active')) {
+                    child.classList.add('active');
+                }
+                break;
+            }else {
+                if (child.classList.contains('active')) {
+                    child.classList.remove('active');
+                }
             }
         }
 
@@ -92,9 +157,4 @@ function changeBackgroundColor(scrollContainer, backgroundCallBack, initialColor
             current = color;
             backgroundCallBack(current);
         }
-    }
-}
-
-function handleNavIconToggle() {
-
-}
+*/
